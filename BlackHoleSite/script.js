134eb8fa4a3bab7d53f587aa9ef70673593664ee
Astrophysicist17 <tommaso13.ale@gmail.com>
2025-03-25 00:48:35 +0100

    update

--- BlackHoleSite/script.js
+++ BlackHoleSite/script.js
@@ -2,46 +2,65 @@ const canvas = document.getElementById("simulationBox");
 const ctx = canvas.getContext("2d");
 canvas.width = 1800;
 canvas.height = 1000;
-const Xp = 700; // Particella in centro a destra
-const Yp = 100; // Particella sopra il centro
-const theta = Math.PI/3;
-const alfa = theta - Math.PI/2; 
+const Xp = 900;
+const Yp = 250;
+const V = 80;
+const G = 6.6e-5;
+const M = 1e9;
+const theta = Math.PI / 6;
+
+let particlePosition = [Xp, Yp];
 let i = 0;
 
 function updateParticlePosition() {
-    const t = i++ / 10;
-    if(t === 0){
-        const Xn = Xp, Yn = Yp;
+    let t = i++/40;
+    if (t === 0) {
+        particlePosition[0] = Xp;
+        particlePosition[1] = Yp;
+    } else {
+        let dx = Math.abs(particlePosition[0] - 900);
+        let dy = Math.abs(particlePosition[1] - 500);
+        let distance = Math.sqrt(dx * dx + dy * dy);
         
-        particlesMove(Xn,Yn);
+        if(distance === 0) return;
+
+        Ax =  (G * M) / (distance * distance);
+        Ay =  (G * M) / (distance * distance);
+        
+        Vx = V * Math.cos(theta) + signAccX(particlePosition[0]) * Ax * t;
+        Vy = V * Math.sin(theta) + signAccY(particlePosition[1]) * Ay * t;
+
+        particlePosition[0] = Xp + Vx * t;
+        particlePosition[1] = Yp + Vy * t;
     }
-    else{
-        const V = 50;
-        const theta = theta + Math.asin((Math.abs(500-Yn))/distance);
-        const alfa = theta - Math.PI/2;
-        const Vx = V * Math.cos(alfa);
-        const Vy = V * Math.sin(alfa);
-        const Xn = Xp + (Vx * t);
-        const Yn = Yp + (Vy * t);
-        const distance = Math.sqrt(Math.pow((Xn-900),2)+Math.pow(Yn-500),2);
+    particlesMove(particlePosition[0], particlePosition[1]);
+}
 
+function signAccX(X){
+    if(X < 900) return 1;
+    else if (X > 900) return -1;
+        else if(X === 900) return 0;
+}
 
-
-
-        particlesMove(Xn,Yn);
-    }
-    
-    
-    
+function signAccY(Y){
+    if(Y < 500) return 1;
+    else if (Y > 500) return -1;
+        else if(Y === 500) return 0;
 }
 
 function particlesMove(x, y) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
+
     ctx.beginPath();
-    ctx.arc(x, y, 2, 0, Math.PI * 2); // Punto visibile
-    ctx.arc(900, 500, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, 2, 0, Math.PI * 2); // Punto mobile
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(900, 500, 10, 0, Math.PI * 2); // Punto centrale
+    ctx.closePath();
     ctx.fill();
 }
 
-setInterval(updateParticlePosition,10);
+setInterval(updateParticlePosition, 10);
